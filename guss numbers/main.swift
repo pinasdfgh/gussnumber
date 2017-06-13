aaba61808da6df4143e07cbbc1b83a9c70c934f7
user <user@userde-MacBook-89.local>
2017-06-13 16:08:32 +0800

    ver0.2

--- guss numbers/main.swift
+++ guss numbers/main.swift
@@ -42,71 +42,104 @@ func checkAB(answer:String,guess:String)->String{
     
     return "\(A)A\(B)B"
 }
-//HW3堅固
-func substring(str:String,start:Int,len:Int)->String{
-    //str.startIndex為什麼不能寫0因為這個欄位的type是string.Index
-    let sindex = str.index(str.startIndex, offsetBy: start)
-    let eindex = str.index(str.startIndex, offsetBy: start + len)
-    let range = sindex..<eindex
-    
-    return str[range]
+func substring(str:String?,start:Int,len:Int)->String{
+    if len > 0 {
+        if let str = str{
+            let sLen = Int(strlen(str))
+            let newstart = start > sLen ?sLen : start
+            let lenth = len > (sLen - newstart) ? (sLen - newstart) : len
+            let Findex = str.index(str.startIndex,offsetBy:newstart)
+            let Eindex = str.index(str.startIndex,offsetBy:newstart + lenth)
+            return str[Findex..<Eindex]
+        }else{
+            return ""
+        }
+    }else{
+        if let str = str{
+            let sLen = Int(strlen(str))
+            let newstart = start > sLen ?sLen : start
+            let Snum = (newstart+len) > 0 ? (newstart+len) : 0
+            let Eindex = str.index(str.startIndex,offsetBy:newstart)
+            let Findex = str.index(str.startIndex,offsetBy:Snum)
+            return str[Findex..<Eindex]
+        }else{
+            return "" 
+        }
+    }
+
 }
 
-//let args = CommandLine.arguments
-//var i = 0
-////for arg in args {
-////    
-////    i += 1
-////    print(i)
-////    print(arg)
-////}
-//
-////print(type(of:args[1]))
-//let d:Int? = Int(args[1])
+//HW3堅固
+//func substring(str:String,start:Int,len:Int)->String{
+//    //str.startIndex為什麼不能寫0因為這個欄位的type是string.Index
+//    let sindex = str.index(str.startIndex, offsetBy: start)
+//    let eindex = str.index(str.startIndex, offsetBy: start + len)
+//    let range = sindex..<eindex
+//    
+//    return str[range]
+//}
+
+let args = CommandLine.arguments
+var i = 0
+//for arg in args {
+//    
+//    i += 1
+//    print(i)
+//    print(arg)
+//}
+
+//print(type(of:args[1]))
+let d:Int? = Int(args[1])
 
 
 //主流程
-let answer = createAnswer(3)
-print(answer)
+let answer = createAnswer(d ?? 0)
+//print(answer)
 var guess:String?
 var isWin:Bool = false
 
 for i in 1...10{
     var isWong:Bool = false
-    print("\(i).請猜一數字", terminator:"")
     repeat{
+        print("\(i).請猜一數字", terminator:"")
+        isWong = false
+        var n = d ?? 0
         guess = readLine()
         
-        print(type(of:guess))
+//        print(type(of:strlen(guess)))
         
-        var check = Set<Int>()
-        if let gus = guess{
-            for ck in 0..<gus.characters.count{
-                if Int(ck) < 10 {
-                    isWong = true
-                }else{
-                  check.insert(Int(ck))
-//                    isWong = true
+//        print(type(of:guess))
+        var check:[Bool] = Array(repeating:false,count:10)
+        if let gus = guess {
+            if var num = Int(gus) {
+                // 		print(num)
+                for i in 0..<n{
+                    var index = num % (10)
+                    num /= 10
+                    // 			print(index)
+                    isWong = check[index]
+                    check[index] = true
                 }
+            }else{
+                isWong = true
             }
         }else{
             isWong = true
         }
-    }while(isWong)
-    
-    
-    
-    
+        var readn = Int(strlen(guess))
+        
+        if readn != n {
+            isWong = true
+        }
+    }while isWong
     //檢查 1.[0-9] 2.no reapt  HW1
-    
     if let gus = guess{
         let result = checkAB(answer: answer, guess: gus)
         print("\(answer)=>\(result)")
-        if result == "\(3)A0B" {
+        if result == "\(d ?? 0)A0B" {
             isWin=true
             break
         }
-        
     }
 }
 
